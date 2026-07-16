ad3949e68882be6df6bddc95bebabb12b8a72b38
murad-ali <limurad42@gmail.com>
2026-07-17 00:14:39 +0400

    Update 0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,5 @@
-def index_range(page: int, page_size: int) -> tuple
+from typing import Tuple
+def index_range(page: int, page_size: int) -> Tuple[int,int]:
 start = (page - 1) * page_size
 end = start + page_size
 return(start, end)
