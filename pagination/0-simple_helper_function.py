64c49ff850313a28293efb808917a3ca201ebca0
murad-ali <limurad42@gmail.com>
2026-07-17 21:34:58 +0400

    Update 0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,3 +1,12 @@
-from typing import Tuple 
-def index_range(page: int, page_size: int) -> Tuple[int,int]:
-    return (page-1)*page_size, page * page_size
+#!/usr/bin/env python3
+"""
+0-simple_helper_function module
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple of size two containing a start index and an end index
+    """
+    return (page - 1) * page_size, page * page_size
