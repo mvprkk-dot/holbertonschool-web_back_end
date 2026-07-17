3554ca32d74864c55d830aa730ddbc96161dcb98
murad-ali <limurad42@gmail.com>
2026-07-17 21:31:20 +0400

    Fix import typo and adjust return statement formatting

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,3 +1,3 @@
-from typing imprt Tuple 
+from typing import Tuple 
 def index_range(page: int, page_size: int) -> Tuple[int,int]:
-    return((page-1)*page_size, page * page_size)
+    return (page-1)*page_size, page * page_size
