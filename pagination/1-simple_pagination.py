d1cfad04c37c85cf265fc6a5ac5976d58d6ddb5c
murad-ali <limurad42@gmail.com>
2026-07-17 21:55:54 +0400

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+"""
+1-simple_pagination module
+"""
 import csv
 import math
 from typing import List, Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
@@ -29,14 +34,17 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Finds the correct indexes to paginate the dataset correctly
+        and returns the appropriate page of the dataset.
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
-      assert type(page) is int and page > 0
-      assert type(page_size) is int and page_size > 0
-      
-      start, end = index_range(page, page_size)
-      data = self.dataset()
-      if start => len(data):
-      
-      return []
-      return data[start:end]
-      
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+
+        if start >= len(data):
+            return []
+
+        return data[start:end]
