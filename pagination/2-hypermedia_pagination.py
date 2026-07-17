1f80e2775e2f43cc642789a40aa7d829b0e3f3ca
murad-ali <limurad42@gmail.com>
2026-07-17 22:10:57 +0400

    Update 2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -48,16 +48,18 @@ class Server:
             return []
 
         return data[start:end]
-      def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Returns a dictionary containing hypermedia pagination information.
+        """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
-        
+
         data = self.get_page(page, page_size)
-        
         total_data = len(self.dataset())
-        total_pages = math.ceil(total_data / page_size
-                                
+        total_pages = math.ceil(total_data / page_size)
+
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
@@ -69,4 +71,3 @@ class Server:
             "prev_page": prev_page,
             "total_pages": total_pages
         }
-        
