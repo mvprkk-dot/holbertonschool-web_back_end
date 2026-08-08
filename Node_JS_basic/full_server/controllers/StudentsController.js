f6ff78e7d3ecab33d4474a2e3dc83ff49291b99c
murad-ali <limurad42@gmail.com>
2026-08-08 18:16:28 +0400

    Create StudentsController.js

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,45 @@
+const readDatabase = require('../utils');
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    const dbPath = process.argv[2];
+
+    readDatabase(dbPath)
+      .then((studentGroups) => {
+        const responseParts = ['This is the list of our students'];
+        const fields = Object.keys(studentGroups).sort((a, b) => a.localeCompare(b));
+
+        for (const field of fields) {
+          const names = studentGroups[field].map((student) => student.firstname);
+          responseParts.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+        }
+
+        response.status(200).send(responseParts.join('\n'));
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const dbPath = process.argv[2];
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(dbPath)
+      .then((studentGroups) => {
+        const students = studentGroups[major] || [];
+        const names = students.map((student) => student.firstname);
+        response.status(200).send(`List: ${names.join(', ')}`);
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+}
+
+module.exports = StudentsController;
