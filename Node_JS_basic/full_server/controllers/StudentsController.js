e074d8b524fbaa3ea0ed3cdc3e04cb34f0be5265
murad-ali <limurad42@gmail.com>
2026-08-08 18:29:27 +0400

    Update StudentsController.js

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,45 +1,38 @@
-const readDatabase = require('../utils');
+static getAllStudents(request, response) {
+  const dbPath = process.argv[2];
 
-class StudentsController {
-  static getAllStudents(request, response) {
-    const dbPath = process.argv[2];
+  readDatabase(dbPath)
+    .then((studentGroups) => {
+      const responseParts = ['This is the list of our students'];
+      const fields = Object.keys(studentGroups).sort((a, b) => a.localeCompare(b));
 
-    readDatabase(dbPath)
-      .then((studentGroups) => {
-        const responseParts = ['This is the list of our students'];
-        const fields = Object.keys(studentGroups).sort((a, b) => a.localeCompare(b));
+      for (const field of fields) {
+        const names = studentGroups[field]; // artıq birbaşa firstname massividir
+        responseParts.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
 
-        for (const field of fields) {
-          const names = studentGroups[field].map((student) => student.firstname);
-          responseParts.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-        }
-
-        response.status(200).send(responseParts.join('\n'));
-      })
-      .catch(() => {
-        response.status(500).send('Cannot load the database');
-      });
-  }
-
-  static getAllStudentsByMajor(request, response) {
-    const dbPath = process.argv[2];
-    const { major } = request.params;
-
-    if (major !== 'CS' && major !== 'SWE') {
-      response.status(500).send('Major parameter must be CS or SWE');
-      return;
-    }
-
-    readDatabase(dbPath)
-      .then((studentGroups) => {
-        const students = studentGroups[major] || [];
-        const names = students.map((student) => student.firstname);
-        response.status(200).send(`List: ${names.join(', ')}`);
-      })
-      .catch(() => {
-        response.status(500).send('Cannot load the database');
-      });
-  }
+      response.status(200).send(responseParts.join('\n'));
+    })
+    .catch(() => {
+      response.status(500).send('Cannot load the database');
+    });
 }
 
-module.exports = StudentsController;
+static getAllStudentsByMajor(request, response) {
+  const dbPath = process.argv[2];
+  const { major } = request.params;
+
+  if (major !== 'CS' && major !== 'SWE') {
+    response.status(500).send('Major parameter must be CS or SWE');
+    return;
+  }
+
+  readDatabase(dbPath)
+    .then((studentGroups) => {
+      const names = studentGroups[major] || [];
+      response.status(200).send(`List: ${names.join(', ')}`);
+    })
+    .catch(() => {
+      response.status(500).send('Cannot load the database');
+    });
+}
