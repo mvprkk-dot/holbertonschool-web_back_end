adac9878d6080ca97ca7ef89ed8162ac042415df
murad-ali <limurad42@gmail.com>
2026-08-08 15:15:44 +0000

    fix readDatabase

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -18,21 +18,17 @@ const readDatabase = (path) => new Promise((resolve, reject) => {
       .split('\n');
 
     const studentGroups = {};
-    const dbFieldNames = fileLines[0].split(',');
-    const studentPropNames = dbFieldNames
-      .slice(0, dbFieldNames.length - 1);
 
     for (const line of fileLines.slice(1)) {
       const studentRecord = line.split(',');
-      const studentPropValues = studentRecord
-        .slice(0, studentRecord.length - 1);
+      const firstname = studentRecord[0];
       const field = studentRecord[studentRecord.length - 1];
+
       if (!Object.keys(studentGroups).includes(field)) {
         studentGroups[field] = [];
       }
-      const studentEntries = studentPropNames
-        .map((propName, idx) => [propName, studentPropValues[idx]]);
-      studentGroups[field].push(Object.fromEntries(studentEntries));
+
+      studentGroups[field].push(firstname);
     }
 
     resolve(studentGroups);
